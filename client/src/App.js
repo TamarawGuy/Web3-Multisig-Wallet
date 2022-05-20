a5cc1095d84c739411a62a6742708544ccaa4f6d
Viktor <viktor.m.rumenov@gmail.com>
2022-05-20 18:31:43 +0200

    Fixed create-react-app and web3 and added components to show state data

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,54 @@
-function App() {
-  return <div>Test</div>;
-}
+import { useEffect, useState } from "react";
+import { getWeb3, getWallet } from "./utils";
+
+import Header from "./Header";
+import NewTransfer from "./NewTransfer";
+
+const App = () => {
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [wallet, setWallet] = useState(undefined);
+  const [approvers, setApprovers] = useState(undefined);
+  const [quorum, setQuorum] = useState(undefined);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = getWeb3();
+      const wallet = await getWallet(web3);
+      const accounts = await web3.eth.getAccounts();
+      const approvers = await wallet.methods.getApprovers().call();
+      const quorum = await wallet.methods.quorum().call();
+      setWeb3(web3);
+      setAccounts(accounts);
+      setWallet(wallet);
+      setApprovers(approvers);
+      setQuorum(quorum);
+    };
+    init();
+  }, []);
+
+  const createTransfer = (transfer) => {
+    wallet.methods
+      .createTransfer(transfer.amount, transfer.to)
+      .send({ from: accounts[0] });
+  };
+
+  if (
+    typeof web3 === "undefined" ||
+    typeof accounts === "undefined" ||
+    typeof wallet === "undefined" ||
+    typeof approvers === "undefined" ||
+    typeof quorum === "undefined"
+  ) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Header approvers={approvers} quorum={quorum} />
+      <NewTransfer createTransfer={createTransfer} />
+    </div>
+  );
+};
 
 export default App;
